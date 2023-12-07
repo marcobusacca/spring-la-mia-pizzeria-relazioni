6d9d9af7bf83ebce323bba01f3dcb17a565ac584
marcobusacca <marcobusacca01@gmail.com>
2023-12-07 11:54:24 +0100

    [FEAT] DiscountController functions

--- src/main/java/org/java/spring/controller/DiscountController.java
+++ src/main/java/org/java/spring/controller/DiscountController.java
@@ -1,8 +1,35 @@
 package org.java.spring.controller;
 
+import java.awt.print.Book;
+import java.time.LocalDate;
+import java.util.List;
+
+import org.java.spring.db.serv.DiscountService;
+import org.java.spring.db.serv.PizzaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class DiscountController {
-
+	
+	@Autowired
+	private PizzaService pizzaService;
+	
+	@Autowired
+	private DiscountService discountService;
+	
+	@GetMapping("/pizzas/discount")
+	public String createDiscount(Model model) {
+		
+		return "";
+	}
+	@PostMapping("/pizzas/discount")
+	public String storeDiscount() {
+		
+		return "";
+	}
 }
