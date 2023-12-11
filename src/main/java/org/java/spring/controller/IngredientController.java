cbe0455b9b7eec593a325c37d2a3d131c85b6c7a
marcobusacca <marcobusacca01@gmail.com>
2023-12-11 15:15:01 +0100

    [FEAT] IngredientController functions for Create and Delete

--- src/main/java/org/java/spring/controller/IngredientController.java
+++ src/main/java/org/java/spring/controller/IngredientController.java
@@ -7,7 +7,13 @@ import org.java.spring.db.serv.IngredientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class IngredientController {
@@ -23,5 +29,50 @@ public class IngredientController {
 		model.addAttribute("ingredients", ingredients);
 		
 		return "ingredients";
+	}
+	
+	@GetMapping("/ingredients/create")
+	public String createIngredient(Model model) {
+		
+		Ingredient ingredient = new Ingredient();
+		
+		model.addAttribute("ingredient", ingredient);
+		
+		return "ingredient-form";
+	}
+	@PostMapping("/ingredients/create")
+	public String storeIngredient(Model model, @Valid @ModelAttribute Ingredient ingredient, BindingResult bindingResult) {
+		
+		return saveIngredient(model, ingredient, bindingResult);
+	}
+	
+	@PostMapping("/ingredients/delete/{id}")
+	public String deleteIngredient(@PathVariable int id) {
+		
+		Ingredient ingredient = ingredientService.findById(id);
+		ingredientService.delete(ingredient);
+		
+		System.out.println(ingredient);
+		
+		return "redirect:/";
+	}
+	
+	private String saveIngredient(Model model, @Valid @ModelAttribute Ingredient ingredient, BindingResult bindingResult) {
+		
+		System.out.println("Ingrediente:\n" + ingredient);
+		System.out.println("\n---------------\n");
+		System.out.println("Error:\n" + bindingResult);
+		
+		if (bindingResult.hasErrors()) {
+			
+			System.out.println(bindingResult);
+			
+			model.addAttribute("ingredient", ingredient);
+			return "ingredient-form";
+		}
+		
+		ingredientService.save(ingredient);
+		
+		return "redirect:/";
 	}
 }
